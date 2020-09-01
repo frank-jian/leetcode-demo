ca408357147680f94c07ddaac55b7c37970414ef
terui <terui@pinduoduo.com>
2020-09-01 19:54:24 +0800

    #6 Z字形变换

--- src/main/java/data/structure/array/ArrayDemo.java
+++ src/main/java/data/structure/array/ArrayDemo.java
@@ -311,6 +311,41 @@ public class ArrayDemo {
     }
 
     /**
+     * #6 Z字形变换(将一个给定字符串根据给定的行数，以从上往下、从左到右进行 Z 字形排列。比如输入字符串为 "LEETCODEISHIRING" 行数为 3 时，排列如下：)
+     * L   C   I   R
+     * E T O E S I I G
+     * E   D   H   N
+     * 之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："LCIRETOESIIGEDHN"。
+     *
+     * @param str
+     * @param rowNum
+     * @return
+     */
+    public String convert(String str, int rowNum) {
+        if (rowNum == 1) {
+            return str;
+        }
+        List<StringBuilder> result = new ArrayList<>();
+        for (int i = 0; i < Math.min(str.length(), rowNum); i++) {
+            result.add(new StringBuilder());
+        }
+        int row = 0;
+        boolean goDown = false;
+        for (char c : str.toCharArray()) {
+            result.get(row).append(c);
+            if (row == 0 || row == rowNum - 1) {
+                goDown = !goDown;
+            }
+            row += goDown ? 1 : -1;
+        }
+        StringBuilder finalResult = new StringBuilder();
+        for (StringBuilder build : result) {
+            finalResult.append(build.toString());
+        }
+        return finalResult.toString();
+    }
+
+    /**
      * 第一步: [1,2,3,4,5,6,7] 0、6
      * 1、len = 7;
      * 最后得到[7,6,5,4,3,2,1]
