f2cf3321d35465519406fe86e6e0560f765fbb49
terui <terui@pinduoduo.com>
2020-09-03 17:51:25 +0800

    #21 合并两个有序链表

--- src/main/java/data/structure/array/LinkedDemo.java
+++ src/main/java/data/structure/array/LinkedDemo.java
@@ -52,4 +52,30 @@ public class LinkedDemo {
         return pre.next;
     }
 
+    /**
+     * #21 合并两个有序链表(将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的)
+     * 输入：1->2->4, 1->3->4
+     * 输出：1->1->2->3->4->4
+     *
+     * @param l1 有序链表1
+     * @param l2 有序链表2
+     * @return 合并链表
+     */
+    public ListNode mergeTwoList(ListNode l1, ListNode l2) {
+        ListNode prehead = new ListNode(-1);
+        ListNode pre = prehead;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                pre.next = l1;
+                l1 = l1.next;
+            } else {
+                pre.next = l2;
+                l2 = l2.next;
+            }
+            pre = pre.next;
+        }
+        pre.next = l1 == null ? l2 : l1;
+        return prehead.next;
+    }
+
 }
