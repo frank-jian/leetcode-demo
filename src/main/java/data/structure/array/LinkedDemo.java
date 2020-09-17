83d358c63bf29d94e52d83ae5743d64864dd6643
terui <terui@pinduoduo.com>
2020-09-17 22:00:47 +0800

    #2 两个链表相加

--- src/main/java/data/structure/array/LinkedDemo.java
+++ src/main/java/data/structure/array/LinkedDemo.java
@@ -1,6 +1,7 @@
 package data.structure.array;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -101,5 +102,42 @@ public class LinkedDemo {
             head = head.next;
         }
         return false;
+    }
+
+    /**
+     * #2 两数相加(给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字)
+     * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+     * 输出：7 -> 0 -> 8
+     * 原因：342 + 465 = 807
+     */
+    public ListNode addTwoNumber(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        ListNode deadNode = new ListNode(0);
+        ListNode currentNode = deadNode;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int a = l1 == null ? 0 : l1.val;
+            int b = l2 == null ? 0 : l2.val;
+            int sum = a + b + carry;
+            carry = sum / 10;
+            currentNode.next = new ListNode(sum % 10);
+            currentNode = currentNode.next;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                l2 = l1.next;
+            }
+        }
+        if (carry > 0) {
+            currentNode.next = new ListNode(carry);
+        }
+        return deadNode.next;
     }
 }
