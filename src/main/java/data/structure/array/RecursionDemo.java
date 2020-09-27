f0ebed251c12740dfd690fbe4cc7e3052f5e597f
terui <terui@pinduoduo.com>
2020-09-27 20:59:57 +0800

    # 剑指offer10-1，斐波那契数列

--- src/main/java/data/structure/array/RecursionDemo.java
+++ src/main/java/data/structure/array/RecursionDemo.java
@@ -1,5 +1,8 @@
 package data.structure.array;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @description: <p>递归Demo</p>
  * @author: terui
@@ -14,7 +17,7 @@ public class RecursionDemo {
         TreeNode right;
 
         public TreeNode(int val) {
-            this.val = value;
+            this.val = val;
         }
     }
 
@@ -74,5 +77,67 @@ public class RecursionDemo {
         //从ans与maxLongestPath中更新最大值
         vas = Math.max(maxLongestPath, vas);
         return maxLongestPath;
+    }
+
+    /**
+     * 写一个函数，输入 n ，求斐波那契（Fibonacci）数列的第 n 项。斐波那契数列的定义如下：
+     * F(0) = 0,   F(1) = 1
+     * F(N) = F(N - 1) + F(N - 2), 其中 N > 1.
+     * 斐波那契数列由 0 和 1 开始，之后的斐波那契数就是由之前的两数相加而得出答案需要取模 1e9+7（1000000007），如计算初始结果为：1000000008，请返回 1。
+     *
+     * @param n
+     * @return
+     */
+    int constant = 1000000007;
+
+    public int fib(int n) {
+        if (n < 2) {
+            return n;
+        }
+        int a = fib(n - 1) % constant;
+        int b = fib(n - 2) % constant;
+        return (a + b) % constant;
+    }
+
+    public int fib2(int n) {
+        return fib(n, new HashMap<>());
+    }
+
+    /**
+     * 非递归方法
+     */
+    public int fib3(int n) {
+        int first = 0;
+        int seconds = 1;
+        while (n > 0) {
+            int temp = (first + seconds) % constant;
+            first = seconds % constant;
+            seconds = temp % constant;
+            n--;
+        }
+        return first;
+    }
+
+
+    private int fib(int n, Map<Integer, Integer> cache) {
+        if (n < 2) {
+            return n;
+        }
+        if (cache.containsKey(n)) {
+            return cache.get(n);
+        }
+        int a = fib(n - 1, cache) % constant;
+        cache.put(n - 1, a);
+        int b = fib(n - 2, cache) % constant;
+        cache.put(n - 2, b);
+
+        int sum = (a + b) % constant;
+        cache.put(n, sum);
+        return sum;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(new RecursionDemo().fib(5));
     }
 }
